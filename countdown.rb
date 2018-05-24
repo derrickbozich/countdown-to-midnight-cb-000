ea117bdd28e2ce564a58b637bc0434babfa54a94
derrick b <dbozich1@gmail.com>
2018-05-24 22:42:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -9,3 +9,14 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 
 end
+
+def countdown_with_sleep(number)
+  until number == 0
+    puts "#{number} SECOND(S)!"
+    sleep 1
+    number -= 1
+  end
+
+  "HAPPY NEW YEAR!"
+
+end
